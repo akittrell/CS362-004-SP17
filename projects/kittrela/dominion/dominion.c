015ab5072425b417c15f77491b254a3f8c9ed1d0
akittrell <a.r.kittrell@gmail.com>
2017-04-12 19:57:41 -0700

    Refactored Smithy into own funciton

--- projects/kittrela/dominion/dominion.c
+++ projects/kittrela/dominion/dominion.c
@@ -670,6 +670,19 @@ int cardAdventurer(struct gameState *state, int currentPlayer){
       return 0;
 }
 
+int cardSmithy (struct gameState *state, int currentPlayer, int handPos){
+    int i;
+      //+3 Cards
+      for (i = 0; i < 3; i++)
+	{
+	  drawCard(currentPlayer, state);
+	}
+
+      //discard card from hand
+      discardCard(handPos, currentPlayer, state, 0);
+    return 0;
+}
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -837,15 +850,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 
     case smithy:
-      //+3 Cards
-      for (i = 0; i < 3; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+        return cardSmithy(state, currentPlayer, handPos);
 
     case village:
       //+1 Card
