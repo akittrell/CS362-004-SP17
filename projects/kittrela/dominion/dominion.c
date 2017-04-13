fbfd58194b1bc82c9eaa16f7718d7403b2787d9d
akittrell <a.r.kittrell@gmail.com>
2017-04-12 20:25:43 -0700

    refactored treasure map card into own function

--- projects/kittrela/dominion/dominion.c
+++ projects/kittrela/dominion/dominion.c
@@ -736,6 +736,38 @@ int cardSalvager(struct gameState *state, int choice1, int currentPlayer, int ha
       return 0;
 }
 
+int cardTreasure_map(struct gameState *state, int currentPlayer, int handPos){
+      int i;
+      //search hand for another treasure_map
+      int index = -1;
+      for (i = 0; i < state->handCount[currentPlayer]; i++)
+	{
+	  if (state->hand[currentPlayer][i] == treasure_map && i != handPos)
+	    {
+	      index = i;
+	      break;
+	    }
+	}
+      if (index > -1)
+	{
+	  //trash both treasure cards
+	  discardCard(handPos, currentPlayer, state, 1);
+	  discardCard(index, currentPlayer, state, 1);
+
+	  //gain 4 Gold cards
+	  for (i = 0; i < 4; i++)
+	    {
+	      gainCard(gold, state, 1, currentPlayer);
+	    }
+
+	  //return success
+	  return 1;
+	}
+
+      //no second treasure_map found in hand
+      return -1;
+}
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -1210,34 +1242,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 
     case treasure_map:
-      //search hand for another treasure_map
-      index = -1;
-      for (i = 0; i < state->handCount[currentPlayer]; i++)
-	{
-	  if (state->hand[currentPlayer][i] == treasure_map && i != handPos)
-	    {
-	      index = i;
-	      break;
-	    }
-	}
-      if (index > -1)
-	{
-	  //trash both treasure cards
-	  discardCard(handPos, currentPlayer, state, 1);
-	  discardCard(index, currentPlayer, state, 1);
-
-	  //gain 4 Gold cards
-	  for (i = 0; i < 4; i++)
-	    {
-	      gainCard(gold, state, 1, currentPlayer);
-	    }
-
-	  //return success
-	  return 1;
-	}
-
-      //no second treasure_map found in hand
-      return -1;
+        return cardTreasure_map(state, currentPlayer, handPos);
     }
 
   return -1;
